c317fe846e40e8f95a4a8f836cc83fcb656dd808
Walker Denial <WalkerDenial@qq.com>
2021-04-24 23:23:16 +0800

    feat: add antd

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,8 +4,6 @@ import { List } from "./list";
 import { cleanObject, useDebounce, useMount } from "utils";
 import { useHttp } from "utils/http";
 
-const apiUrl = process.env.REACT_APP_API_URL;
-
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
   const [param, setParam] = useState({
