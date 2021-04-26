ffde5af5ac2d10f4f9ef532803e8a9d270c3be4a
Walker Denial <WalkerDenial@qq.com>
2021-04-27 00:11:01 +0800

    feat: useDocumentTitle

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -46,3 +46,19 @@ export const useArray = <T>(initialArray: T[]) => {
     },
   };
 };
+
+export const useDocumentTitle = (title: string, keepOnUnmount: boolean = false) => {
+  const oldTitle = document.title
+  useEffect(() => {
+    document.title = title
+  }, [title])
+
+  useEffect(() => {
+    return () => {
+      if (!keepOnUnmount) {
+        document.title = oldTitle
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+}
