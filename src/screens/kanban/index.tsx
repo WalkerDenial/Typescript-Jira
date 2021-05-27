c503c21b841d523e9064337b09e316b6e34f9298
Walker Denial <WalkerDenial@qq.com>
2021-05-27 23:40:55 +0800

    feat: dev kanban and task

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -1,30 +1,48 @@
 import { useDocumentTitle } from "utils";
 import { useKanbans } from "utils/kanban";
 import { KanbanColumn } from "./kanban-colums";
-import { useKanbanSearchParams, useProjectInUrl } from "./util";
+import {
+  useKanbanSearchParams,
+  useProjectInUrl,
+  useTaskSearchParams,
+} from "./util";
 import styled from "@emotion/styled";
 import { SearchPanel } from "./search-panel";
 import { ScreenContainer } from "components/lib";
+import { useTasks } from "utils/task";
+import { Spin } from "antd";
+import { CreateKanban } from "./create-kanban";
 
 export const KanbanScreen = () => {
   useDocumentTitle("看板列表");
   const { data: currentProject } = useProjectInUrl();
-  const { data: kanbans } = useKanbans(useKanbanSearchParams());
+  const { data: kanbans, isLoading: kanbanIsLoading } = useKanbans(
+    useKanbanSearchParams()
+  );
+  const { isLoading: taskIsLoading } = useTasks(useTaskSearchParams());
+  const isLoading = taskIsLoading || kanbanIsLoading;
   return (
     <ScreenContainer>
       <h1>{currentProject?.name}看板</h1>
       <SearchPanel />
-      <ColumnsContainer>
-        {kanbans?.map((kanban) => (
-          <KanbanColumn kanban={kanban} key={kanban.id} />
-        ))}
-      </ColumnsContainer>
+
+      {isLoading ? (
+        <Spin size={"large"} />
+      ) : (
+        <ColumnsContainer>
+          {kanbans?.map((kanban) => (
+            <KanbanColumn kanban={kanban} key={kanban.id} />
+          ))}
+          <CreateKanban />
+        </ColumnsContainer>
+      )}
     </ScreenContainer>
   );
 };
 
-const ColumnsContainer = styled.div`
+export const ColumnsContainer = styled.div`
   display: flex;
   overflow-x: scroll;
   flex: 1;
+  overflow: hidden;
 `;
