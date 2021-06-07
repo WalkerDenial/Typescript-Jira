d7c359b13031d6e262f7b7f0fc31f2f927e89058
Walker Denial <WalkerDenial@qq.com>
2021-06-07 23:31:03 +0800

    feat: add test

---
+++ src/__tests__/http.ts
@@ -0,0 +1,49 @@
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { http } from "utils/http";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+const server = setupServer();
+
+// jest 是对 react 最友好的一个测试库
+// beforeAll 代表执行所有的测试之前，先来执行一下回调函数
+beforeAll(() => server.listen());
+
+// 每一个测试跑完以后，都重置 mock 路由
+afterEach(() => server.resetHandlers());
+
+// 所有的测试跑完后，关闭 mock 路由
+afterAll(() => server.close());
+
+test("http 方法发送异步请求", async () => {
+  const endpoint = "test-endpoint";
+  const mockResult = { mockValue: "mock" };
+
+  server.use(
+    rest.get(`${apiUrl}/${endpoint}`, (req, res, ctx) =>
+      res(ctx.json(mockResult))
+    )
+  );
+
+  const result = await http(endpoint);
+  expect(result).toEqual(mockResult);
+});
+
+test("http 请求时会在 header 里带上 token", async () => {
+  const token = "FAKE_TOKEN";
+  const endpoint = "test-endpoint";
+  const mockResult = { mockValue: "mock" };
+
+  let request: any;
+
+  server.use(
+    rest.get(`${apiUrl}/${endpoint}`, async (req, res, ctx) => {
+      request = req;
+      return res(ctx.json(mockResult));
+    })
+  );
+
+  await http(endpoint, { token });
+  expect(request.headers.get("Authorization")).toBe(`Bearer ${token}`);
+});
